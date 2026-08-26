e442f645df3a28790b873f4cdf9840ecb93dddbf
sujeetunbeatable <sujeetunbeatable@gmail.com>
2026-08-26 10:42:38 +0100

    fix: remove PWA plugin for Cloudflare Pages compatibility

--- next.config.ts
+++ next.config.ts
@@ -1,20 +1,7 @@
-import withPWAInit from "@ducanh2912/next-pwa";
 import { NextConfig } from "next/dist/server/config";
 
-const withPWA = withPWAInit({
-  dest: "public",
-  register: true,
-  disable: process.env.NODE_ENV === "development",
-  reloadOnOnline: true,
-  cacheOnFrontEndNav: true,
-  aggressiveFrontEndNavCaching: true,
-  workboxOptions: {
-    disableDevLogs: true,
-  },
-});
-
 const nextConfig: NextConfig = {
-  // https://github.com/payloadcms/payload/issues/12550#issuecomment-2939070941
+  output: "standalone",
   turbopack: {
     resolveExtensions: [".mdx", ".tsx", ".ts", ".jsx", ".js", ".mjs", ".json"],
   },
@@ -24,6 +11,4 @@ const nextConfig: NextConfig = {
   },
 };
 
-const pwa = withPWA(nextConfig);
-
-export default pwa;
+export default nextConfig;
